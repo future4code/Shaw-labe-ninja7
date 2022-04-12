3c99c5d0a569ed6b609dc93256f40f7182ba95f7
unknown <stellaneves84@gmail.com>
2022-04-11 21:40:14 -0300

    stella-pag-inicial

--- src/components/paginaInicial.js
+++ src/components/paginaInicial.js
@@ -4,7 +4,19 @@ export class paginaInicial extends Component {
   render() {
     return (
       <div>
-        <p>Pronto para começar!</p>
+        <header>
+          <h1>labeNinjas</h1>
+          <button>Home</button>
+          <button>Carrinho</button>
+        </header>
+
+        <main>
+          <h3>O talento certo no momento certo</h3>
+          <button>Quero ser um ninja</button>
+          <button>Contratar um ninja</button>
+          </main>
+
+          <footer></footer>
       </div>
     )
   }
