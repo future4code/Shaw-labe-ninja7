59599a17ec2099a3c1e432d5c3db5d10a5a103a3
unknown <matheus-lb@hotmail.com>
2022-04-14 16:47:51 -0300

    tentando resolver conflitos na pag inicial

--- src/components/PaginaIncial.js
+++ src/components/PaginaIncial.js
@@ -4,11 +4,6 @@ export default class PaginaInicial extends React.Component {
   render() {
     return (
       <div>
-      <header>
-        <h1>labeNinjas</h1>
-        <button onClick={()=>this.props.trocar("home")}>Home</button>
-        <button onClick={()=>this.props.trocar("carrinho")}>Carrinho</button>
-      </header>
 
       <main>
         <h3>O talento certo no momento certo</h3>
