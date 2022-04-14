5523046fdfa49b5a4ff55ca4e7938a613fadbe5d
unknown <matheus-lb@hotmail.com>
2022-04-14 16:08:59 -0300

    feito troca de paginas

--- src/App.js
+++ src/App.js
@@ -7,19 +7,50 @@ import Cadastro from "./components/Cadastro";
 import Servicos from "./components/Servicos";
 import Card from "./components/Card";
 import CarrinhoPage from "./components/CarrinhoPage";
+import DetalhesDoServico from './components/DetalhesDoServico'
 
 export default class extends React.Component{
 
+	state = {
+		paginaAtual:""
+	}
+
+	trocaTela = (tela) =>{
+		this.setState({paginaAtual:tela})
+	} 
+
+	
+
 	render(){
 
-		// utilizando o link base e a autorização
-		console.log(linkApi)
-		console.log(aut)
+		console.log(this.state.paginaAtual)
+
+		let pagina 
+
+		switch(this.state.paginaAtual){
+			case "inicial":
+				pagina = <PaginaInicial trocar={this.trocaTela}/>
+				break;
+			case "cadastro" :
+				pagina = <Cadastro />
+				break;
+			case "carrinho" :
+				pagina = <CarrinhoPage />
+				break;
+			case "detalhes" :
+				pagina = <DetalhesDoServico />
+				break;
+			case "servicos" :
+				pagina = <Servicos />
+				break;
+			default:
+				pagina = <PaginaInicial trocar={this.trocaTela}/>
+				break;
+		}
+		
 		return(
 			<div>
-			 <PaginaInicial/>
-			 <Servicos />
-			 <CarrinhoPage/>
+				{pagina}
 			</div>
 		)
 	}
